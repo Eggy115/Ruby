ab465a99c3dc307dd25d05715eb6213d79ada1b8
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-18 20:08:34 +0100

    Add files via upload

---
+++ core_ext/match_data/names.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# NOTE remove once implemented in Opal; see https://github.com/opal/opal/issues/1964
+class MatchData
+  def names
+    []
+  end
+end unless MatchData.method_defined? :names
